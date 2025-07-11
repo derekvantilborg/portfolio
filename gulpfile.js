13f92a688c10058934899c1c8c89a577f1307459
derekvantilborg <dwvantilborg@gmail.com>
2025-07-11 21:33:12 +0200

    css tryout

--- gulpfile.js
+++ gulpfile.js
@@ -40,7 +40,7 @@ gulp.task('browser-sync', ['jekyll-build'], function() {
 * Compile and minify sass
 */
 gulp.task('sass', function() {
-  	gulp.src('src/styles/**/*.scss')
+  	gulp.src('assets/css/**/*.scss')
 		.pipe(plumber())
 		.pipe(sass())
 		.pipe(csso())
